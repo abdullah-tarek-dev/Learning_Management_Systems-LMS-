71b7ad4505fe3f3dc120ea1ab2cd080f062406c8
Abdullah Darwish <engabdullahtarekdarwish@gmail.com>
2026-06-06 12:13:43 +0300

    add folders

--- LMS.Application/obj/Debug/net8.0/LMS.Application.AssemblyInfo.cs
+++ LMS.Application/obj/Debug/net8.0/LMS.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LMS.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e03210c2512c4c6cab5a642572272d4e1d902f43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d20cdd96d4321bfd66f26142ff02b17d470d57d")]
 [assembly: System.Reflection.AssemblyProductAttribute("LMS.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LMS.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
